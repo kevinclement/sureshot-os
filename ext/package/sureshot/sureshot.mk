72edcc8d9b65a2d969f80c7cd9e65bce16b072b8
Kevin Clement <kevin.clement@gmail.com>
2022-10-03 09:48:33 +0000

    add source code for sureshot

---
+++ ext/package/sureshot/sureshot.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# SureShot Pinball
+#
+################################################################################
+
+# Pull code from github release
+SURESHOT_VERSION = 0.1
+SURESHOT_SITE = $(call github,kevinclement,sureshot,v$(SURESHOT_VERSION))
+
+define SURESHOT_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/code/sureshot	
+	cp -dpfR $(@D)/* $(TARGET_DIR)/code/sureshot
+endef
+
+$(eval $(generic-package))
+
