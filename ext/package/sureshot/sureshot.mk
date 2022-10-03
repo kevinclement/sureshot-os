61da336858e764ce6ce6aed827a9a8a900c8cd4c
Kevin Clement <kevin.clement@gmail.com>
2022-10-03 11:21:56 +0000

    update to ss 0.3 and install sysinit

--- ext/package/sureshot/sureshot.mk
+++ ext/package/sureshot/sureshot.mk
@@ -5,12 +5,16 @@
 ################################################################################
 
 # Pull code from github release
-SURESHOT_VERSION = 0.1
+SURESHOT_VERSION = 0.3
 SURESHOT_SITE = $(call github,kevinclement,sureshot,v$(SURESHOT_VERSION))
 
 define SURESHOT_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/code/sureshot	
 	cp -dpfR $(@D)/* $(TARGET_DIR)/code/sureshot
+
+	$(INSTALL) -D -m 0755 $(@D)/systemd/S30sureshot \
+  						  $(TARGET_DIR)/etc/init.d/S30sureshot
+
 endef
 
 $(eval $(generic-package))
