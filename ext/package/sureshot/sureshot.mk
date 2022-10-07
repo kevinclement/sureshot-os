4e36d12d4766c04122783fbcc629fc24df6b7c81
Kevin Clement <kevin.clement@gmail.com>
2022-10-07 05:10:51 +0000

    moving daemon to this repo

--- ext/package/sureshot/sureshot.mk
+++ ext/package/sureshot/sureshot.mk
@@ -11,10 +11,6 @@ SURESHOT_SITE = $(call github,kevinclement,sureshot,v$(SURESHOT_VERSION))
 define SURESHOT_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/code/sureshot	
 	cp -dpfR $(@D)/* $(TARGET_DIR)/code/sureshot
-
-	$(INSTALL) -D -m 0755 $(@D)/systemd/S30sureshot \
-  						  $(TARGET_DIR)/etc/init.d/S30sureshot
-
 endef
 
 $(eval $(generic-package))
