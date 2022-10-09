7bc2a4de5494c5c6c46ad9396495b33c2238bd0f
Kevin Clement <kevin.clement@gmail.com>
2022-10-09 05:05:20 +0000

    move sureshot init to sureshot package

--- ext/package/sureshot/sureshot.mk
+++ ext/package/sureshot/sureshot.mk
@@ -9,9 +9,10 @@ SURESHOT_VERSION = 0.6
 SURESHOT_SITE = $(call github,kevinclement,sureshot,v$(SURESHOT_VERSION))
 
 define SURESHOT_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/code/sureshot	
-	cp -dpfR $(@D)/* $(TARGET_DIR)/code/sureshot
-
+	$(INSTALL) -m 0777 -d $(@D) $(TARGET_DIR)/code/sureshot
+	$(INSTALL) -m 0755 -D $(SURESHOT_PKGDIR)/S00sureshot \
+		$(TARGET_DIR)/etc/init.d/S00sureshot
+	
 	mkdir -p $(TARGET_DIR)/code/sureshot/data
 	mkdir -p $(TARGET_DIR)/code/sureshot/logs
 endef
